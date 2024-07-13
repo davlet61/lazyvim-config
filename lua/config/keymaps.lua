1bd321e43a13c05482f352f38964ae046deeb67a
Dovlat Yakshibayev <dovlat93@gmail.com>
2024-07-13 21:11:30 +0200

    remap windows which-key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,7 +7,27 @@ local del = vim.keymap.del
 -- Delete defaults
 del("n", "<leader>ft")
 del("n", "<leader>fT")
+del("n", "<leader>ww")
+del("n", "<leader>wd")
+del("n", "<leader>wm")
+del("n", "<leader>w-")
+del("n", "<leader>w|")
+del("n", "<leader>-")
+del("n", "<leader>|")
 
-map("n", "<leader>ww", "<cmd>w<cr>", { desc = "Save" })
+map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save" })
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
+
+-- windows
+map("n", "<leader>pw", "<C-W>p", { desc = "Other window", remap = true })
+map("n", "<leader>pd", "<C-W>c", { desc = "Delete Window", remap = true })
+map("n", "<leader>p-", "<C-W>s", { desc = "Split window below", remap = true })
+map("n", "<leader>p|", "<C-W>v", { desc = "Split window right", remap = true })
+map("n", "<leader>p+", "<C-W>v", { desc = "Split window right", remap = true })
+map("n", "<leader>-", "<C-W>s", { desc = "Split window below", remap = true })
+map("n", "<leader>|", "<C-W>v", { desc = "Split window right", remap = true })
+map("n", "<leader>+", "<C-W>v", { desc = "Split window right", remap = true })
+map("n", "<leader>pm", function()
+  LazyVim.toggle.maximize()
+end, { desc = "Maximize Toggle" })
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    spec = {
+      {
+        mode = { "n", "v" },
+        { "<leader>w", desc = "", hidden = true },
+        { "<leader>p", group = "windows" },
+      },
+    },
+  },
+}
