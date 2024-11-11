a525a55f4f801003935fd5bac6f009e44d0f8385
Dovlat <dovlat93@gmail.com>
2024-11-11 08:01:56 +0100

    config term

--- lua/plugins/vim-dadbod.lua
+++ lua/plugins/vim-dadbod.lua
@@ -8,5 +8,6 @@ return {
   init = function()
     vim.g.db_ui_use_nerd_fonts = 1
     vim.g.db_ui_win_position = "right"
+    vim.g.db = "postgresql://postgres:postgres@localhost:5437/whitson-pvt"
   end,
 }
